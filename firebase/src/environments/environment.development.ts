0644627bb7e13d54071169c36d7ffc05b3cd6a89
cherryApp <j.cserko@gmail.com>
2023-02-09 13:04:22 +0100

    init firebase app

---
+++ firebase/src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    // Your web app's Firebase configuration
+    firebaseConfig: {
+        apiKey: "AIzaSyAKIwjto-pXmuhLW5-qx6dVbUML_eMz7Lg",
+        authDomain: "angular-firebase-9e3a2.firebaseapp.com",
+        databaseURL: "https://angular-firebase-9e3a2.firebaseio.com",
+        projectId: "angular-firebase-9e3a2",
+        storageBucket: "angular-firebase-9e3a2.appspot.com",
+        messagingSenderId: "479113064636",
+        appId: "1:479113064636:web:00fb9fa38adce741d220b9"
+    }
+};
